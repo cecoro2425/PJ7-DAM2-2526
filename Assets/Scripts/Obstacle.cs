4bceb42b727205747c88d46713059d828defcd5a
JordiAlbas <jordialbaschiavola@gmail.com>
2026-04-16 20:37:51 +0200

    si

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,18 +4,15 @@ public class Obstacle : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform checkPoint;
-    [SerializeField] private GestionVidas2 gestionVidas2;
+
 
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "Penguin")
         {
-            Debug.Log("Player: " + player);
             Debug.Log("CheckPoint: " + checkPoint);
-            Debug.Log("GestionVidas2: " + gestionVidas2);
 
-            gestionVidas2.PerdreVida(1);
             player.position = checkPoint.position;
         }
     }
